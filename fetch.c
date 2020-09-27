de0a1ef709e50e564d8274ff40cd8394bdd03ab4
13-CF <65169288+13-CF@users.noreply.github.com>
2020-09-27 18:30:55 +0000

    reimplement package finder
    Add ascii logo system using structs, support for shells located in /usr/bin and some fixes for gentoo

--- fetch.c
+++ fetch.c
@@ -24,6 +24,22 @@
 #define CYAN "\033[0;36m"
 #define WHITE "\033[0;37m"
 
+typedef struct distinfo {
+	char * dcol1;
+	char * dcol2;
+	char * dcol3;
+	char * dcol4;
+	char * dcol5;
+	char * dcol6;
+	char * dcol7;
+	char * dcol8;
+	char * getpkg;
+} Dist;
+
+
+
+
+
 char * os()
 {
 	FILE *f;
@@ -32,7 +48,7 @@ char * os()
 	
 	f = fopen("/etc/os-release", "r");
 	if (f == NULL) {
-		f = fopen("/var/run/os-release", "r"); } //for *BSD
+		f = fopen("/var/run/os-release", "r"); }
 		if (f == NULL) {
 			return "unknown"; }
 //	fscanf(f, "%s", dist);
@@ -42,54 +58,121 @@ char * os()
 	str = strtok(dist, "NAME=\""); //if dist is Arch Linux, it seems to return rch Linux, so the next to lines fix it
 	if (strncmp(str, "rch Linux", 9) == 0) {
 		str = "Arch Linux"; }
+	else if (strncmp(str, "Gentoo\n", 7) == 0) {
+		str = "Gentoo"; }
 	return str;
 }
 
 
-char * pkg() {
-	char *distro = os();
-	
-	if (strncmp(distro, "void", 4) == 0) {
-		return "xbps-query -l | wc -l"; }
-	else if (strncmp(distro, "Fedora", 6) == 0) {
-		return "rpm -qa | wc -l"; }
-	else if (strncmp(distro, "Arch Linux", 10) == 0) { //something I'm doing seems to make 'Arch Linux' not work. I think it might be the strtok() in the os() function
-		return "pacman -Qq | wc -l"; }
-	else if (strncmp(distro, "Gentoo", 6) == 0) {
-		return "equery list \"*\""; } //not sure if this is how to do it on gentoo
-	else if (strncmp(distro, "FreeBSD", 7) == 0) {
-		return "pkg list -i | wc -l"; }//I think this is the correct command? 
+Dist asciiart() {
+	char * dist = os();
+	Dist info;
+	if (strncmp(dist, "void", 4) == 0) {	
+		info.dcol1 =    BGREEN "     _______\n";
+		info.dcol2 =    BGREEN  "_ \\______ -";
+		info.dcol3 =	BGREEN  " | \\  ___  \\ |";
+		info.dcol4 = 	BGREEN" | | /   \\ | |";
+		info.dcol5 =	BGREEN" | | \\___/ | |";
+		info.dcol6 =	BGREEN" | \\______ \\_|";
+		info.dcol7  =	BGREEN "  -_______\\ "; 
+		info.dcol8  =   "";
+		info.getpkg = "xbps-query -l | wc -l"; }
+	else if (strncmp(dist, "Gentoo", 6) == 0) {
+       		info.dcol1=BWHITE"     .---. \n";
+      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol3=BWHITE"    \\.@-@./     ";
+      		info.dcol4=BWHITE"    /`\\_/`\\     ";
+    		info.dcol5=BWHITE"   //  _  \\\\    ";
+   		info.dcol6=BWHITE"  | \\     )|_   ";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol8=BWHITE" \\__/'---'\\__/";
+		info.getpkg = "equery list \"*\" | wc -l"; }
+	else if (strncmp(dist, "Arch Linux", 10)==0) {
+       		info.dcol1=BWHITE"     .---. \n";
+      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol3=BWHITE"    \\.@-@./     ";
+      		info.dcol4=BWHITE"    /`\\_/`\\     ";
+    		info.dcol5=BWHITE"   //  _  \\\\    ";
+   		info.dcol6=BWHITE"  | \\     )|_   ";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol8=BWHITE" \\__/'---'\\__/";
+		info.getpkg = "pacman -Qq | wc -l";  }
+	else if (strncmp(dist, "Fedora", 6)==0) {
+       		info.dcol1=BWHITE"     .---. \n";
+      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol3=BWHITE"    \\.@-@./     ";
+      		info.dcol4=BWHITE"    /`\\_/`\\     ";
+    		info.dcol5=BWHITE"   //  _  \\\\    ";
+   		info.dcol6=BWHITE"  | \\     )|_   ";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol8=BWHITE" \\__/'---'\\__/";
+		info.getpkg="rpm -qa | wc -l";
+	}
+	else if (strncmp(dist, "FreeBSD", 7)==0) {
+       		info.dcol1=BWHITE"     .---. \n";
+      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol3=BWHITE"    \\.@-@./     ";
+      		info.dcol4=BWHITE"    /`\\_/`\\     ";
+    		info.dcol5=BWHITE"   //  _  \\\\    ";
+   		info.dcol6=BWHITE"  | \\     )|_   ";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol8=BWHITE" \\__/'---'\\__/";
+		info.getpkg="pkg list -i | wc -l";
+	}
 
-	
-	return "echo unknown";
+	else {
+                
+       		info.dcol1=BWHITE"     .---. \n";
+      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol3=BWHITE"    \\.@-@./     ";
+      		info.dcol4=BWHITE"    /`\\_/`\\     ";
+    		info.dcol5=BWHITE"   //  _  \\\\    ";
+   		info.dcol6=BWHITE"  | \\     )|_   ";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol8=BWHITE" \\__/'---'\\__/";
+		info.getpkg = "echo unknown";
+	}
+	return info;
 }
 
-	
+
+
+char * shell() {
+	char * shell = strtok(getenv("SHELL"), "/bin");
+
+	//if your $SHELL is set to /usr/bin/*sh this will be run
+	if (strncmp(shell, "usr", 3) == 0){
+		while (strncmp(shell, "usr", 3)==0) {
+			shell = strtok(NULL, "bin/");
+		}
+	}
+	return shell;
+}
+
 int main(){
 	/* initialise system info */
 	struct sysinfo si; //used for uptime
 	struct utsname ui; //used for hostname, system name and system release
 	uname(&ui);
 	sysinfo(&si);
-   /* Print info + void logo (\033 is used for colours) */
-	printf(BGREEN"     _______\n" );
-	printf(BGREEN"  _ \\______ -      USER %s%s\n",WHITE, getenv("USER"));
-	printf(BGREEN"| \\  ___  \\ |    DISTRO %s%s\n",WHITE,os());
-	printf(BGREEN"| | /   \\ | |    KERNEL %s%s %s\n", WHITE, ui.sysname, ui.release);
-	printf(BGREEN"| | \\___/ | |    UPTIME %s%lih %lim\n", WHITE, si.uptime / 3600, (si.uptime / 60) - (si.uptime / 3600 * 60));
-	printf(BGREEN"| \\______ \\_|     SHELL %s%s\n",WHITE, strtok(getenv("SHELL"), "/bin")); //if you want the shell path, remove the strtok
-	//system("echo \" -_______\\     \033[1;33mPACKAGES\033[0;37m $(xbps-query -l | wc -l) (xbps)\n\""); 
-	//printf(GREEN" -_______\\     %s \n", pkg());
+	Dist ascii = asciiart();
 
-	// code that runs the code that the pkg() function returns 
+	printf("%s", ascii.dcol1);
+	printf("  %s       USER %s%s\n",ascii.dcol2, WHITE, getenv("USER"));
+	printf("%s    DISTRO %s%s\n",ascii.dcol3,WHITE,os());
+	printf("%s    KERNEL %s%s %s\n",ascii.dcol4, WHITE, ui.sysname, ui.release);
+	printf("%s    UPTIME %s%lih %lim\n", ascii.dcol5, WHITE, si.uptime / 3600, (si.uptime / 60) - (si.uptime / 3600 * 60));
+	printf("%s     SHELL %s%s\n",ascii.dcol6,WHITE, shell());
+
 	FILE *pkgs;
 	char p;
-	pkgs = popen(pkg(), "r");
+	pkgs = popen(ascii.getpkg, "r");
 	if (pkgs == NULL) {
 		puts("\0"); }
-	printf(BGREEN" -_______\\         PKGS "WHITE);
+	printf("%s        PKGS "WHITE, ascii.dcol7);
 	while ( (p=fgetc(pkgs)) != EOF) {
 		putchar(p); }
+	printf("%s", ascii.dcol8);
 	printf("\n");
 	return 0;
 }
