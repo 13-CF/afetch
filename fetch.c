8cf5f82a1921a90e4ce4992a6574173d4780efa6
13-CF <65169288+13-CF@users.noreply.github.com>
2020-09-28 15:55:02 +0000

    Add ascii art for more distros + easier text align

--- fetch.c
+++ fetch.c
@@ -60,7 +60,7 @@ char * os()
 		str = "Arch Linux"; }
 	else if (strncmp(str, "Gentoo\n", 7) == 0) {
 		str = "Gentoo"; }
-	else if (strncmp(str, "rtix Linux", 10) == 0) {
+	else if (strncmp(str, "rtix Linux\n", 10) == 0) {
 		str = "Artix Linux"; }
 	return str;
 }
@@ -71,69 +71,75 @@ Dist asciiart() {
 	Dist info;
 	if (strncmp(dist, "void", 4) == 0) {	
 		info.dcol1 =    BGREEN "     _______\n";
-		info.dcol2 =    BGREEN  "_ \\______ -";
+		info.dcol2 =    BGREEN  "  _ \\______ - ";
 		info.dcol3 =	BGREEN  " | \\  ___  \\ |";
 		info.dcol4 = 	BGREEN" | | /   \\ | |";
 		info.dcol5 =	BGREEN" | | \\___/ | |";
 		info.dcol6 =	BGREEN" | \\______ \\_|";
-		info.dcol7  =	BGREEN "  -_______\\ "; 
+		info.dcol7  =	BGREEN "  -_______\\   "; 
 		info.dcol8  =   "";
 		info.getpkg = "xbps-query -l | wc -l"; }
 	else if (strncmp(dist, "Gentoo", 6) == 0) {
        		info.dcol1=BMAGENTA"   _-----_ \n";
-      		info.dcol2=BMAGENTA"(       \\ ";
+      		info.dcol2=BMAGENTA"  (       \\  ";
       		info.dcol3=BMAGENTA"  \\    0   \\ ";
       		info.dcol4=BMAGENTA"   \\        )";
     		info.dcol5=BMAGENTA"   /      _/ ";
    		info.dcol6=BMAGENTA"  (     _-   ";
-   		info.dcol7=BMAGENTA"  \\____-   ";
+   		info.dcol7=BMAGENTA"  \\____-     ";
    		info.dcol8=BWHITE"\n";
 		info.getpkg = "ls -d /var/db/pkg/*/* | wc -l"; }
 	else if (strncmp(dist, "Arch Linux", 10)==0) {
-       		info.dcol1=BWHITE"     .---. \n";
-      		info.dcol2=BWHITE"  /     \\    ";
-      		info.dcol3=BWHITE"    \\.@-@./     ";
-      		info.dcol4=BWHITE"    /`\\_/`\\     ";
-    		info.dcol5=BWHITE"   //  _  \\\\    ";
-   		info.dcol6=BWHITE"  | \\     )|_   ";
-   		info.dcol7=BWHITE" /`\\_`>  <_/ \\\n";
-   		info.dcol8=BWHITE" \\__/'---'\\__/";
+       		info.dcol1=BCYAN"";
+      		info.dcol2=BCYAN"      /\\      ";
+      		info.dcol3=BCYAN"     /  \\     ";
+      		info.dcol4=BCYAN"    /\\   \\    ";
+    		info.dcol5=BCYAN"   /      \\   ";
+   		info.dcol6=BCYAN"  /   ,,   \\  ";
+   		info.dcol7=BCYAN" /   |  |  -\\ ";
+   		info.dcol8=BCYAN"/_-''    ''-_\\\n";
+		info.getpkg = "pacman -Qq | wc -l";  }
+	else if (strncmp(dist, "Artix Linux", 11)==0) {
+       		info.dcol1=BCYAN"";
+      		info.dcol2=BCYAN"      /\\      ";
+      		info.dcol3=BCYAN"     /  \\     ";
+      		info.dcol4=BCYAN"    /`'.,\\    ";
+    		info.dcol5=BCYAN"   /     ',   ";
+   		info.dcol6=BCYAN"  /      ,`\\  ";
+   		info.dcol7=BCYAN" /   ,.'`.  \\ ";
+   		info.dcol8=BCYAN"/.,'`     `'.\\\n";
 		info.getpkg = "pacman -Qq | wc -l";  }
 	else if (strncmp(dist, "Fedora", 6)==0) {
-       		info.dcol1=BWHITE"     .---. \n";
-      		info.dcol2=BWHITE"  /     \\    ";
-      		info.dcol3=BWHITE"    \\.@-@./     ";
-      		info.dcol4=BWHITE"    /`\\_/`\\     ";
-    		info.dcol5=BWHITE"   //  _  \\\\    ";
-   		info.dcol6=BWHITE"  | \\     )|_   ";
-   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
-   		info.dcol8=BWHITE" \\__/'---'\\__/\n";
-		info.getpkg="rpm -qa | wc -l";
-	}
+       		info.dcol1=BBLUE"      _____\n";
+      		info.dcol2=BBLUE"     /   __)\\ ";
+      		info.dcol3=BBLUE"     |  /  \\ \\";
+      		info.dcol4=BBLUE"  ___|  |__/ /";
+    		info.dcol5=BBLUE" / (_    _)_/ ";
+   		info.dcol6=BBLUE"/ /  |  |     ";
+   		info.dcol7=BBLUE"\\ \\__/  |     ";
+   		info.dcol8=BBLUE" \\(_____/";
+		info.getpkg="rpm -qa | wc -l";	}
 	else if (strncmp(dist, "FreeBSD", 7)==0) {
-       		info.dcol1=BWHITE"     .---. \n";
-      		info.dcol2=BWHITE"  /     \\    ";
-      		info.dcol3=BWHITE"    \\.@-@./     ";
-      		info.dcol4=BWHITE"    /`\\_/`\\     ";
-    		info.dcol5=BWHITE"   //  _  \\\\    ";
-   		info.dcol6=BWHITE"  | \\     )|_   ";
-   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
-   		info.dcol8=BWHITE" \\__/'---'\\__/\n";
-		info.getpkg="pkg list -i | wc -l";
-	}
+       		info.dcol2=BRED"/\\,-'''''-,/\\";
+      		info.dcol3=BRED"\\_)       (_/";
+      		info.dcol4=BRED"|           |";
+      		info.dcol5=BRED"|           |";
+    		info.dcol6=BRED" ;         ; ";
+   		info.dcol7=BRED"  '-_____-'  ";
+   		info.dcol8=BRED"";
+   		info.dcol1=BRED"";
+		info.getpkg="pkg info | wc -l"; }
 
 	else {
-                
        		info.dcol1=BWHITE"     .---. \n";
-      		info.dcol2=BWHITE"  /     \\    ";
+      		info.dcol2=BWHITE"    /     \\     ";
       		info.dcol3=BWHITE"    \\.@-@./     ";
       		info.dcol4=BWHITE"    /`\\_/`\\     ";
     		info.dcol5=BWHITE"   //  _  \\\\    ";
    		info.dcol6=BWHITE"  | \\     )|_   ";
-   		info.dcol7=BWHITE" /`\\_`>  <_/ \\";
+   		info.dcol7=BWHITE" /`\\_`>  <_/ \\  ";
    		info.dcol8=BWHITE" \\__/'---'\\__/\n";
-		info.getpkg = "echo unknown";
-	}
+		info.getpkg = "echo unknown";}
 	return info;
 }
 
@@ -160,18 +166,18 @@ int main(){
 	Dist ascii = asciiart();
 
 	printf("%s", ascii.dcol1);
-	printf("  %s       USER %s%s\n",ascii.dcol2, WHITE, getenv("USER"));
+	printf("%s      USER %s%s\n",ascii.dcol2, WHITE, getenv("USER"));
 	printf("%s    DISTRO %s%s\n",ascii.dcol3,WHITE,os());
 	printf("%s    KERNEL %s%s %s\n",ascii.dcol4, WHITE, ui.sysname, ui.release);
 	printf("%s    UPTIME %s%lih %lim\n", ascii.dcol5, WHITE, si.uptime / 3600, (si.uptime / 60) - (si.uptime / 3600 * 60));
 	printf("%s     SHELL %s%s\n",ascii.dcol6,WHITE, shell());
+	printf("%s      PKGS "WHITE, ascii.dcol7);
 
 	FILE *pkgs;
 	char p;
 	pkgs = popen(ascii.getpkg, "r");
 	if (pkgs == NULL) {
 		puts("\0"); }
-	printf("%s        PKGS "WHITE, ascii.dcol7);
 	while ( (p=fgetc(pkgs)) != EOF) {
 		putchar(p); }
 	printf("%s", ascii.dcol8);
