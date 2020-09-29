3c2b74f88bb860fda2832b4b8a9d676e25c77c50
cf <cf.1303@protonmail.com>
2020-09-29 17:49:36 +0100

    add config file to configure at compile time

---
+++ config.h
@@ -0,0 +1,41 @@
+// used to change the text displayed, text boxes should all align
+#define USERTEXT    "       USER"
+#define DISROTEXT   "     DISTRO"
+#define KERNELTEXT  "     KERNEL" 
+#define UPTIMETEXT  "     UPTIME"
+#define SHELLTEXT   "      SHELL"
+#define PACKAGETEXT "       PKGS"
+
+
+	/* BRIGHT */
+#define BBLACK "\033[1;30m"
+#define BRED "\033[1;31m"
+#define BGREEN "\033[1;32m"
+#define BYELLOW "\033[1;33m"
+#define BBLUE "\033[1;34m"
+#define BMAGENTA "\033[1;35m"
+#define BCYAN "\033[1;36m"
+#define BWHITE "\033[1;37m"
+	/* NORMAL */
+#define BLACK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[0;37m"
+
+
+//probably don't change this (it'll break shit)
+typedef struct distinfo {
+	char * dcol1;
+	char * dcol2;
+	char * dcol3;
+	char * dcol4;
+	char * dcol5;
+	char * dcol6;
+	char * dcol7;
+	char * dcol8;
+	char * getpkg;
+} Dist;
--- fetch.c
+++ fetch.c
@@ -3,42 +3,7 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 #include <sys/utsname.h>
-
-/* COLOUR CODES */
-	/* BRIGHT */
-#define BBLACK "\033[1;30m"
-#define BRED "\033[1;31m"
-#define BGREEN "\033[1;32m"
-#define BYELLOW "\033[1;33m"
-#define BBLUE "\033[1;34m"
-#define BMAGENTA "\033[1;35m"
-#define BCYAN "\033[1;36m"
-#define BWHITE "\033[1;37m"
-	/* NORMAL */
-#define BLACK "\033[0;30m"
-#define RED "\033[0;31m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[0;33m"
-#define BLUE "\033[0;34m"
-#define MAGENTA "\033[0;35m"
-#define CYAN "\033[0;36m"
-#define WHITE "\033[0;37m"
-
-typedef struct distinfo {
-	char * dcol1;
-	char * dcol2;
-	char * dcol3;
-	char * dcol4;
-	char * dcol5;
-	char * dcol6;
-	char * dcol7;
-	char * dcol8;
-	char * getpkg;
-} Dist;
-
-
-
-
+#include "config.h"
 
 char * os()
 {
@@ -187,12 +152,12 @@ int main(){
 	Dist ascii = asciiart();
 
 	printf("%s", ascii.dcol1);
-	printf("%s      USER %s%s\n",ascii.dcol2, WHITE, getenv("USER"));
-	printf("%s    DISTRO %s%s\n",ascii.dcol3,WHITE,os());
-	printf("%s    KERNEL %s%s %s\n",ascii.dcol4, WHITE, ui.sysname, ui.release);
-	printf("%s    UPTIME %s%lih %lim\n", ascii.dcol5, WHITE, si.uptime / 3600, (si.uptime / 60) - (si.uptime / 3600 * 60));
-	printf("%s     SHELL %s%s\n",ascii.dcol6,WHITE, shell());
-	printf("%s      PKGS "WHITE, ascii.dcol7);
+	printf("%s %s %s%s\n",ascii.dcol2,USERTEXT, WHITE, getenv("USER"));
+	printf("%s %s %s%s\n",ascii.dcol3,DISROTEXT, WHITE,os());
+	printf("%s %s %s%s %s\n",ascii.dcol4,KERNELTEXT, WHITE, ui.sysname, ui.release);
+	printf("%s %s %s%lih %lim\n", ascii.dcol5,UPTIMETEXT, WHITE, si.uptime / 3600, (si.uptime / 60) - (si.uptime / 3600 * 60));
+	printf("%s %s %s%s\n",ascii.dcol6, SHELLTEXT,WHITE, shell());
+	printf("%s %s %s",ascii.dcol7, PACKAGETEXT, WHITE);
 
 	FILE *pkgs;
 	char p;
