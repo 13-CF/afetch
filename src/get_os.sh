e7f051734628d9d590d14941919fb18f902fea1e
Saghya <saghyattila77@gmail.com>
2022-03-12 18:27:15 +0100

    strip versions from distros

--- src/get_os.sh
+++ src/get_os.sh
@@ -278,10 +278,10 @@ main() {
 
     sed -i "s/.*#define DISTRO.*/#define DISTRO \"$DISTRO\"/" src/config.h
     sed -i "s/.*#define GET_PKG_CNT.*/#define GET_PKG_CNT \"$PACKAGES | wc -l\"/" src/config.h
-    if grep -q "$DISTRO" src/distros.txt; then
+    if grep -q "${DISTRO%% [0-9]*}" src/distros.txt; then
         sed -i -e '/#define ASCII_ART/,+8d' src/config.h
         echo "#define ASCII_ART \\" >> src/config.h
-        sed -n "/$DISTRO/,+8p" src/distros.txt | sed '1d' >> src/config.h
+        sed -n "/${DISTRO%% [0-9]*}/,+8p" src/distros.txt | sed '1d' >> src/config.h
     fi
 }
 
