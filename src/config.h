64aabd2c12be60651a7a0d481b1dda040a75ecde
Saghya <saghyattila77@gmail.com>
2022-03-12 18:25:08 +0100

    more sane defaults

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
-#define OS_TEXT      "  " // "     OS "
-#define KERNEL_TEXT  "  " // " KERNEL "
-#define UPTIME_TEXT  "  " // " UPTIME "
-#define SHELL_TEXT   "  " // "  SHELL "
-#define PACKAGE_TEXT "  " // "   PKGS "
-#define COLOR_TEXT   "  "
+#define OS_TEXT      "OS      " // "  "
+#define KERNEL_TEXT  "KERNEL  " // "  "
+#define UPTIME_TEXT  "UPTIME  " // "  "
+#define SHELL_TEXT   "SHELL   " // "  "
+#define PACKAGE_TEXT "PKGS    " // "  "
+#define COLOR_TEXT   ""         // "  "
 
 /* See options in color.h file */
 #define TEXT_COLOR     BGREEN
@@ -20,14 +20,14 @@
 */
 
 /* These lines are automatically set by get_os.sh */
-#define DISTRO "Distro"
+#define DISTRO      "Distro"
 #define GET_PKG_CNT ""
 #define ASCII_ART \
-    .row1 = BWHITE "",                \
-    .row2 = BGREEN "      ___     ",  \
-    .row3 = BGREEN "  ___/   \\___ ", \
-    .row4 = BGREEN " /   '---'   \\", \
-    .row5 = BGREEN " '--_______--'",  \
-    .row6 = BWHITE "      / \\     ", \
-    .row7 = BWHITE "     /   \\    ", \
-    .row8 = BWHITE "    /     \\   "
+    .row1 = BGREEN "      ___     ",  \
+    .row2 = BGREEN "  ___/   \\___ ", \
+    .row3 = BGREEN " /   '---'   \\", \
+    .row4 = BGREEN " '--_______--'",  \
+    .row5 = BWHITE "      / \\     ", \
+    .row6 = BWHITE "     /   \\    ", \
+    .row7 = BWHITE "    /     \\   ", \
+    .row8 = BWHITE "               "
