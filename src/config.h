c5f5d2cc6571af1205c2a66313040f5bfb53e9cf
Saghya <saghyattila77@gmail.com>
2022-03-20 20:54:24 +0100

    change defaults

--- src/config.h
+++ src/config.h
@@ -1,13 +1,12 @@
-#define OS_TEXT      "OS      " // "  "
-#define KERNEL_TEXT  "KERNEL  " // "  "
-#define UPTIME_TEXT  "UPTIME  " // "  "
-#define SHELL_TEXT   "SHELL   " // "  "
-#define PACKAGE_TEXT "PKGS    " // "  "
-#define MEMORY_TEXT  "MEMORY  " // "  "
-#define COLOR_TEXT   ""         // "  "
+#define OS_TEXT      "os      "
+#define KERNEL_TEXT  "kernel  "
+#define UPTIME_TEXT  "uptime  "
+#define SHELL_TEXT   "shell   "
+#define PACKAGE_TEXT "pkgs    "
+#define MEMORY_TEXT  "memory  "
 
 /* See options in color.h file */
-#define TEXT_COLOR     BGREEN
+#define TEXT_COLOR     GREEN
 #define VARIABLE_COLOR BMAGENTA
 
 #define PRINT_HOST      1 // 0 == false, anything else is true
@@ -18,7 +17,7 @@
 #define PRINT_SHELL     0
 #define PRINT_MEMORY    1 
 #define PRINT_COLORS    1
-#define COLOR_CHARACTER "● "
+#define COLOR_CHARACTER "███"
 
 /* Some examples of things you may want to use for your COLOR_CHARACTER
 
@@ -31,11 +30,11 @@
 #define DISTRO      "Distro"
 #define GET_PKG_CNT ""
 #define ASCII_ART \
-    .row1 = BGREEN "      ___     ",  \
-    .row2 = BGREEN "  ___/   \\___ ", \
-    .row3 = BGREEN " /   '---'   \\", \
-    .row4 = BGREEN " '--_______--'",  \
-    .row5 = BWHITE "      / \\     ", \
-    .row6 = BWHITE "     /   \\    ", \
-    .row7 = BWHITE "    /     \\   ", \
-    .row8 = BWHITE "               "
+    .row1 = BGREEN "      ___       ",  \
+    .row2 = BGREEN "  ___/   \\___   ", \
+    .row3 = BGREEN " /   '---'   \\  ", \
+    .row4 = BGREEN " '--_______--'  ",  \
+    .row5 = BWHITE "      / \\       ", \
+    .row6 = BWHITE "     /   \\      ", \
+    .row7 = BWHITE "    /     \\     ", \
+    .row8 = BWHITE "                 "
